7785b4fa8873b50ff6d1a777ded314936ee471cb
Bruno Oliveira <brunooliveira.unb@gmail.com>
2024-08-23 00:29:42 -0300

    adicionando form de orçamento

--- frontend/src/Containers/Orçamento/Orçamento.js
+++ frontend/src/Containers/Orçamento/Orçamento.js
@@ -1,14 +1,59 @@
 import React from "react";
 import "./Orçamento.css";
 import Grid from "@mui/material/Grid";
-
-import Background from "../../assets/orçamento.png";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 
 function Orçamento() {
   return (
     <div className="Orçamento">
       <Grid container spacing={2}>
-        <Grid item md={6}></Grid>
+        <Grid item md={6} style={{ marginTop: "256px" }}>
+          <Box component="form" noValidate autoComplete="off">
+            <TextField
+              id="email"
+              label="EMAIL"
+              variant="standard"
+              fullWidth
+              required
+              InputProps={{
+                disableUnderline: false,
+              }}
+            />
+            <TextField
+              id="assunto"
+              label="ASSUNTO"
+              variant="standard"
+              fullWidth
+              required
+              style={{ marginTop: "24px" }}
+              InputProps={{
+                disableUnderline: false,
+              }}
+            />
+            <TextField
+              id="mensagem"
+              label="MENSAGEM"
+              variant="standard"
+              multiline
+              rows={10}
+              fullWidth
+              required
+              style={{ marginTop: "24px" }}
+              InputProps={{
+                disableUnderline: false,
+              }}
+            />
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "#2c2c2c" }}
+              fullWidth
+            >
+              Enviar
+            </Button>
+          </Box>
+        </Grid>
         <Grid item md={6}></Grid>
       </Grid>
     </div>
